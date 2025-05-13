938a287912708396e5d20ad02a48c39a1c00c7db
JesusOrtega07 <jesus.ortega07@unach.mx>
2025-05-13 00:20:33 -0600

    Start

---
+++ Bimbo/Bimbo/BimboApp.swift
@@ -0,0 +1,17 @@
+//
+//  BimboApp.swift
+//  Bimbo
+//
+//  Created by Jesus Ortega on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BimboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
